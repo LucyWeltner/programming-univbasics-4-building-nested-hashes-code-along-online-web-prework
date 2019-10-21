e843637c54856bb68ce75f53c6bd459e309b647d
Lucy Weltner <lucyiweltner@gmail.com>
2019-10-21 01:17:24 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -20,7 +20,7 @@ def third_challenge
         :name => "Lady Capulet", :age => 51
         },
       :heroine => {
-        
+        :name => "Juliet", :age => 14, :status => "Alive"
         },
       :heroine_friends => []
    }
