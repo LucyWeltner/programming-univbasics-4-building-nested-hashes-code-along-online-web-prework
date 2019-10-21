92462de7a5a851904dd51389eb9e818feba6576f
Lucy Weltner <lucyiweltner@gmail.com>
2019-10-21 01:10:53 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,26 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = 
+   {:monague => 
+      {:patriarch => {
+      :name => "Lord Montague", :age => 53
+      }, :matriarch => {:name => "Lady Montague", :age => 54
+      }, :hero => {
+        :name => "Romeo", :age => 15, :status => "Alive"}, :herosfriends => [
+          :Benvolio => {
+            :name => "Benvolio", :age => 17, :attitude => "worried"
+        }, 
+          :Mercutio => {:name => "Mercutio", :age => 18, :attitude => "hot headed"
+        }
+      ]
+    }, :capulet => {
+      :patriarch => {
+        :name => "Lord Capulet", :age => 50
+      }
+      :matriarch => {:name => "Lady Capulet", :age => 51}
+      :hero => {:name => "Juliet", :age => 14, :status => "Alive"}
+      :herosfriends => [:Steven => {:name => "Steven", :age => 30, :attitude => "confused", :Nurse => {:name => "Nurse", :age => 44, :attitude => "Sassy"
+        }
+      ]
+    }
+  }
 end
